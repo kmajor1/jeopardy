586605a1413c48996c9b9811105ffe470fb522fb
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2019-03-07 20:01:18 -0500

    added mongoose connection

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// import mongoose 
+const mongoose = require('mongoose');
+
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// connect to mongoose 
+mongoose.connect("mongodb://localhost/jeopardy", { useNewUrlParser: true });
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
