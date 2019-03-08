96f34c00d3f886f02977796cf152eab059194264
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2019-03-07 20:41:02 -0500

    added some basic new components

---
+++ client/src/components/Gameboard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Fragment} from 'react'
+import {Container} from 'react-bootstrap'
+import {Button} from 'react-bootstrap'
+// this is the gameBoard where questions/categories go
+
+class Gameboard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isStarted: false 
+    }
+  }
+
+  render(){
+    return (
+      <div></div>
+    )
+  }
+  }
+
+  export default Gameboard; 
