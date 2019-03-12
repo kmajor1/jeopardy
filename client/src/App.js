04bde0d3e48d0784c37495d2e9a8a193336545ed
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2019-03-12 18:51:24 -0400

    additional boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import {Container} from 'react-bootstrap'
 import './App.css';
 import Gameboard from './components/Gameboard'
+import Scoreboard from './components/ScoreBoard'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Container></Container>
+        <Container>
+          <Scoreboard />
+        </Container>
 
       </div>
     );
---
+++ client/src/components/ScoreBoard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Button} from 'antd';
+
+export default function Scoreboard(props){
+  return (
+    <div>
+      <Button>Test Button</Button>
+    </div>
+  )
+}
+
