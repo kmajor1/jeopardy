ee7608d6351fa68ab0ca87dc8b94d729acdcf6b2
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2019-03-19 19:18:09 -0400

    updated question schema

--- models/question.js
+++ models/question.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const questionSchema = new Schema({
-  _id: Schema.Types.ObjectId, 
   question: {
     type: String, 
     required: true
