b61cfb49ad9d7efac2783af5f7e6352934113d7c
jcho <john92cho@hotmail.ca>
2019-03-19 17:35:18 -0400

    25 questions

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -13,14 +13,38 @@ router.get("/category", (req, res) => {
       categories.push(response.data[i].id)
     }
     return categories
-  }).then(stored => category(stored))
-  .then(response => res.end()) // instead of a console log here, pass response to a controller
+  }).then(quesions = (categories) => {
+
+
+    for (i = 0; i < categories.length; i++) {
+      axios.get("http://jservice.io/api/category?id=" + categories[i]).then(response => {
+        console.log(response.data.clues[0].question + " question 1")
+        console.log(response.data.clues[0].answer)
+        console.log("---------------------------------------------")
+        
+        console.log(response.data.clues[1].question + " question 2")
+        console.log(response.data.clues[1].answer)
+        console.log("---------------------------------------------")
+
+        console.log(response.data.clues[2].question + " question 3")
+        console.log(response.data.clues[2].answer)
+        console.log("---------------------------------------------")
+
+        console.log(response.data.clues[3].question + " question 4")
+        console.log(response.data.clues[3].answer)
+        console.log("---------------------------------------------")
+
+        console.log(response.data.clues[4].question + " question 5")
+        console.log(response.data.clues[4].answer)
+      })
+    }
+  })
+    // .then(response => res.end()) // instead of a console log here, pass response to a controller
     .catch(err => res.status(422).json(err))
 
 
 });
 
-router.get("/")
 
 module.exports = router;
 
