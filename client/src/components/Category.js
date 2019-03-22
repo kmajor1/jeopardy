ee6ec36bbc0ae18f470160a9ee600760ac66dfda
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2019-03-21 20:23:13 -0400

    jeocolors

--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -24,7 +24,7 @@ class Category extends React.Component {
     return (
       <Fragment>
         <tr>
-          <td className="question-tile">{this.props.children}</td>
+          <td className="category-tile question-tile">{this.props.children}</td>
           {this.props.tiles.map((value,index) => (<Question key={index} question={value.question} answer={value.answer} showQuestion={showQuestion} answerQuestion={this.props.answerQuestion}>{this.props.doubleJeopardy ? doubleJeopardy[index] : jeopardy[index]}</Question>))}
           
         </tr>
