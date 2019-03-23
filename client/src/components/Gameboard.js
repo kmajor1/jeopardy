382340d9542b193fe22376107716db87912d3e8d
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2019-03-22 22:56:24 -0400

    deleted isStarted state var

--- client/src/components/Gameboard.js
+++ client/src/components/Gameboard.js
@@ -14,7 +14,6 @@ class Gameboard extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      isStarted: false,
       boardView: true,
       currentQuestion: '',
       currentAnswer: '',
